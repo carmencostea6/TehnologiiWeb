b362bba5025ed5669bb51e7d422e3d3151d77e3b
carmencostea6 <carmencostea6@gmail.com>
2024-12-13 22:45:35 +0200

    Removed TehnologiiWeb from staging

---
+++ javascript/menu.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const hamburger = document.querySelector('.hamburger');
+    const navbar = document.querySelector('.navbar');
+  
+    hamburger.addEventListener('click', () => {
+      navbar.classList.toggle('active'); // Afișează/ascunde meniul
+    });
+  });
